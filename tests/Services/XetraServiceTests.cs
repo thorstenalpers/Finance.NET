5a0fd76f182dd9e010a1ec2638de1724d444600f
thorstenalpers <thorstenalpers@web.de>
2024-12-13 09:22:59 +0100

    Fix unit test

--- tests/Services/XetraServiceTests.cs
+++ tests/Services/XetraServiceTests.cs
@@ -57,8 +57,11 @@ public class XetraServiceTests
 	public async Task GetTradableInstruments_WithValidEntries_ReturnsResult()
 	{
 		// Arrange
-		var filePath = Path.Combine(Directory.GetCurrentDirectory(), "TestData", "Xetra", "t7-xetr-allTradableInstruments.csv");
-		SetupHttpCsvFileResponse(filePath);
+		var csvFilePath = Path.Combine(Directory.GetCurrentDirectory(), "TestData", "Xetra", "t7-xetr-allTradableInstruments.csv");
+		var htmlFilePath = Path.Combine(Directory.GetCurrentDirectory(), "TestData", "Xetra", "xetra-download-area.html");
+
+		SetupHttpResponses(htmlFilePath, csvFilePath);
+
 		var service = new XetraService(
 			_mockLogger.Object,
 			_mockHttpClientFactory.Object,
@@ -74,13 +77,24 @@ public class XetraServiceTests
 		Assert.That(result.All(e => !string.IsNullOrWhiteSpace(e.Mnemonic)));
 	}
 
-	private void SetupHttpCsvFileResponse(string filePath)
+	private void SetupHttpResponses(string htmlFilePath, string csvFilePath)
 	{
-		var jsonContent = File.ReadAllText(filePath);
+		var htmlFilePathContent = File.ReadAllText(htmlFilePath);
+		var csvFileContent = File.ReadAllText(csvFilePath);
+
+		_mockHandler
+					.Protected()
+			.Setup<Task<HttpResponseMessage>>("SendAsync",
+									 ItExpr.Is<HttpRequestMessage>(req => req.RequestUri.AbsoluteUri.EndsWith(".csv", System.StringComparison.OrdinalIgnoreCase)),
+									 ItExpr.IsAny<CancellationToken>())
+			.ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(csvFileContent, Encoding.UTF8, "text/csv") });
+
 		_mockHandler
 			.Protected()
-			.Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-			.ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(jsonContent, Encoding.UTF8, "text/csv") });
+			.Setup<Task<HttpResponseMessage>>("SendAsync",
+									 ItExpr.Is<HttpRequestMessage>(req => req.RequestUri.AbsoluteUri.StartsWith(Constants.XetraInstrumentsUrl, System.StringComparison.OrdinalIgnoreCase)),
+									 ItExpr.IsAny<CancellationToken>())
+			.ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK) { Content = new StringContent(htmlFilePathContent, Encoding.UTF8, "text/html") });
 		_mockHttpClientFactory.Setup(e => e.CreateClient(It.IsAny<string>())).Returns(new HttpClient(_mockHandler.Object));
 	}
 }
